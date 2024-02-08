73fc642399d872f9cbfcfb1f93779d955bcac56f
ddll <1728964316@qq.com>
2024-02-08 14:05:11 +0800

    基础功能

--- Song.py
+++ Song.py
@@ -1,7 +1,17 @@
 from datetime import datetime
 
 class Song:
-    def __init__(self, title, path,ChangeTime):
+    def __init__(self, title='未知', path='', modification_time=0, artist='未知'):
         self.title = title
         self.path = path
-        self.ChangeTime=datetime.fromtimestamp(ChangeTime)
+        # 检查modification_time是否为整数或浮点数类型的时间戳
+        if isinstance(modification_time, (int, float)):
+            # 如果是整数或浮点数，假设它是时间戳，并转换为ISO8601格式的字符串
+            self.modification_time = datetime.fromtimestamp(modification_time).strftime("%Y-%m-%d %H:%M:%S")
+        elif isinstance(modification_time, str):
+            # 如果已经是字符串，直接使用
+            self.modification_time = modification_time
+        else:
+            # 如果modification_time既不是整数/浮点数也不是字符串，或为了处理其他类型
+            self.modification_time = "1970-01-01 00:00:00"  # 或其他合适的默认值
+        self.artist = artist
