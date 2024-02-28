ad6f953c59e14472ea7ec74829543f67b5cf8296
ddll <1728964316@qq.com>
2024-02-28 20:41:18 +0800

    基础功能

--- ClickableSlider.py
+++ ClickableSlider.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from PyQt5.QtWidgets import QSlider
 from PyQt5.QtCore import Qt
 
@@ -27,33 +27,3 @@ class ClickableSlider(QSlider):
         # 将点击位置转换为滑块的值
         value = self.minimum() + (clickPos / sliderLength) * valueRange
         return value
-=======
-from PyQt5.QtWidgets import QSlider
-from PyQt5.QtCore import Qt
-
-class ClickableSlider(QSlider):
-    def mousePressEvent(self, event):
-        super().mousePressEvent(event)  # 调用父类的事件处理以保持滑块拖动功能
-        try:
-            if event.button() == Qt.LeftButton:  # 仅在左键点击时处理
-                val = self.pixelPosToRangeValue(event.pos())
-                self.setValue(int(val))
-                self.sliderReleased.emit()
-
-        except Exception as e:
-            print(f"mousePressEvent: {e}")
-
-    def pixelPosToRangeValue(self, pos):
-        # 计算点击的像素位置对应的滑块值
-        if self.orientation() == Qt.Horizontal:
-            valueRange = self.maximum() - self.minimum()
-            sliderLength = self.width()
-            clickPos = pos.x()
-        else:  # 垂直滑块的情况
-            valueRange = self.maximum() - self.minimum()
-            sliderLength = self.height()
-            clickPos = pos.y()
-        # 将点击位置转换为滑块的值
-        value = self.minimum() + (clickPos / sliderLength) * valueRange
-        return value
->>>>>>> 6baf4e0 (基础功能)
